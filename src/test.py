e9cb14ea3a9ca10b9665cdf724157b64cd0ccede
AGachelin <159674898+AGachelin@users.noreply.github.com>
2024-02-17 21:05:24 +0100

    bug enregistrement

--- src/test.py
+++ src/test.py
@@ -621,18 +621,19 @@ class fenetre_d_ajout(QWidget):
             for i in epices:
                 if i not in self.all_tags:
                     query.exec(
-                        f"""alter table recettes add column {i} Text default("")"""
+                        f"""alter table recettes add column '{i}' Text default('')"""
                     )
                 insertion_query = insertion_query + "," + i
                 name = name + "," + "'épice'"
             for i in tags:
                 if i not in self.all_tags:
                     query.exec(
-                        f"""alter table recettes add column {i} Text default("")"""
+                        f"""alter table recettes add column '{i}' Text default('')"""
                     )
                 insertion_query = insertion_query + "," + i
                 name = name + "," + "'tag'"
             insertion_query = insertion_query + ") VALUES (" + name + ")"
+            print(insertion_query)
             query.exec(f"""{insertion_query}""")
             query2 = query2[:-1] + query2_bis[:-1] + ")"
             query.exec(f"""{query2}""")
