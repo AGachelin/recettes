0c224daef8039800590ecee4be6ad16b4384d27b
AGachelin <159674898+AGachelin@users.noreply.github.com>
2024-02-20 23:51:39 +0100

    bug recherche par tag

--- src/main_search.py
+++ src/main_search.py
@@ -85,16 +85,18 @@ class main_search(QWidget):
                 request = request + f'"{names[i]}"' + "=0 and "
         checkstates_bis = [i.checked() for i in self.search_menus[1].getWidgets()]
         names = self.search_menus[1].getWidgetNames()
+        print(checkstates_bis)
         for i in range(0, len(checkstates_bis)):
-            if checkstates_bis[i] == 2:
-                request = request + f'"{names[i]}" <> 0 and '
+            if checkstates_bis[i] == (0, 0):
+                request = request + f'"{names[i]}" <> \'\' and '
             elif checkstates_bis[i] == 0:
-                request = request + f'"{names[i]}" = 0 and '
+                request = request + f'"{names[i]}" = \'\' and '
         request = request[:-5]
         if len(request) < len("SELECT id,nom from recettes where "):
             request = f"SELECT id,nom from recettes where nom like {t}"
         else:
             request = request + " and nom like " + t
+        print(request)
         query.exec(f"""{request}""")
         while query.next():
             btn = QPushButton(self.inner)
